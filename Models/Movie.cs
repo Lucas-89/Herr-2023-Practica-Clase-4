3aec5820b457fbecfba7024f701d1759bd9b3c3e
Lucas Mansilla <mansillalucas1212@gmail.com>
2023-04-07 11:05:39 -0300

    Herr. Practica Clase 4

---
+++ Models/Movie.cs
@@ -0,0 +1,14 @@
+using System.ComponentModel.DataAnnotations;
+
+namespace Clase4.Models;
+
+public class Movie{
+    public string Code{get;set;}
+    [Display(Name="Nombre")]
+    [Required]
+    public string Name{get;set;}
+    public int Minutes{get;set;}
+    public string Category{get;set;}
+
+    public string Review{get;set;}
+}
---
+++ Services/MovieService.cs
@@ -0,0 +1,41 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Clase4.Models;
+
+namespace Clase4.Services;
+
+public static class MovieService{
+    static List<Movie> Movies {get;set;}
+
+    static MovieService(){
+        Movies = new List<Movie>
+        {
+            new Movie{Name="Back to the Future", Code="BTF", Category="Sci FY", Minutes=120, Review="OK"},
+            new Movie{Name="Avatar", Code="AVT", Category="Sci FY", Minutes=500, Review="ASD"},
+            new Movie{Name="Hannibal", Code="HNL", Category="Terror", Minutes=90, Review="Hannibal nunca pestanea"}
+        };
+    }
+    public static List<Movie> GetAll() => Movies;
+
+    public static void Add(Movie obj){
+        if (obj == null)
+        {
+            return;
+        }
+        Movies.Add(obj);
+    }
+
+    public static Movie? Get(string code) => Movies.FirstOrDefault(x=>x.Code.ToLower()==code.ToLower());
+
+    public static void Delete(string code){
+        var movieToDelete= Get(code);
+        if (movieToDelete != null)
+        {
+            Movies.Remove(movieToDelete);
+        }
+    }
+
+
+}
